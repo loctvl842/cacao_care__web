a644a05acdf4b4c17e4d262b44ba7ee18e302589
loctvl842 <loclepnvx@gmail.com>
2023-03-26 13:25:00 +0700

    feat: add

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,23 @@
 import styles from "./style.module.scss";
 import classNames from "classnames/bind";
 
+// icons
+import { HiOutlineBell } from "react-icons/hi";
+
 let cx = classNames.bind(styles);
 
 const Header = () => {
   return (
     <div className={cx("container")}>
-      <h1>Header</h1>
+      <div className={cx("left")}>
+        <img src="/logo.png" alt="" />
+      </div>
+      <div className={cx("right")}>
+        <div className={cx("notification")}>
+          <HiOutlineBell />
+          <div className={cx("badge")}></div>
+        </div>
+      </div>
     </div>
   );
 };
