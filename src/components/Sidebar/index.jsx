2b27231161a9af80b8be35b351a76896237cb4d5
loctvl842 <loclepnvx@gmail.com>
2023-03-26 14:36:43 +0700

    feat: add

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,72 @@
+import styles from "./style.module.scss";
+import classNames from "classnames/bind";
+
+// icons
+import { BsHouse, BsGraphUp, BsToggleOff } from "react-icons/bs";
+import { FiSettings } from "react-icons/fi";
+import { SiAdafruit } from "react-icons/si";
+
+let cx = classNames.bind(styles);
+
+const Sidebar = () => {
+  return (
+    <div className={cx("container")}>
+      <div className={cx("menu")}>
+        <div className={cx("title")}>
+          <h1>Menu</h1>
+        </div>
+        <ul className={cx("list")}>
+          <li className={cx("item", "active")}>
+            <div className={cx("option-wrapper")}>
+              <div className={cx("icon")}>
+                <BsHouse />
+              </div>
+              <span>Overview</span>
+            </div>
+          </li>
+          <li className={cx("item")}>
+            <div className={cx("option-wrapper")}>
+              <div className={cx("icon")}>
+                <BsGraphUp />
+              </div>
+              <span>Statistics</span>
+            </div>
+          </li>
+          <li className={cx("item")}>
+            <div className={cx("option-wrapper")}>
+              <div className={cx("icon")}>
+                <BsToggleOff />
+              </div>
+              <span>Controls</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className={cx("menu")}>
+        <div className={cx("title")}>
+          <h1>Account</h1>
+        </div>
+        <ul className={cx("list")}>
+          <li className={cx("item")}>
+            <div className={cx("option-wrapper")}>
+              <div className={cx("icon")}>
+                <SiAdafruit />
+              </div>
+              <span>Adafruit IO</span>
+            </div>
+          </li>
+          <li className={cx("item")}>
+            <div className={cx("option-wrapper")}>
+              <div className={cx("icon")}>
+                <FiSettings />
+              </div>
+              <span>Settings</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,17 @@
+import styles from "./style.module.scss";
+import classNames from "classnames/bind";
+
+// components
+import { Sidebar } from "~/components";
+
+let cx = classNames.bind(styles);
+
 const Home = () => {
   return (
-    <div>
-      <p>Home page</p>
+    <div className={cx("container")}>
+      <div className={cx("wrapper")}>
+        <Sidebar />
+      </div>
     </div>
   );
 };
