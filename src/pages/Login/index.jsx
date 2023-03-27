c3b45bc09ecb545af2c502a65f3af25410d9d0d1
loctvl842 <loclepnvx@gmail.com>
2023-03-28 01:14:23 +0700

    feat: add  page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,34 @@
+import styles from "./style.module.scss";
+import classNames from "classnames/bind";
+
+let cx = classNames.bind(styles);
+
 const Login = () => {
   return (
-    <div>
-      <p>Login page</p>
+    <div className={cx("container")}>
+      <div className={cx("left")}>
+        <div className={cx("form-wrapper")}>
+          <form className={cx("form-data")}>
+            <div className={cx("form-control")}>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+            <div className={cx("form-control")}>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Active Key"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className={cx("right")}>
+        <img src="/login_img.jpeg" alt="" />
+      </div>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
+// layout
 import { MainLayout } from "~/layout";
 
+// pages
 import Home from "~/pages/Home";
+import Login from "~/pages/Login";
 
 const pages = [
   { path: ["/", "/home"], components: <Home />, layout: MainLayout },
+  { path: "/login", components: <Login /> },
 ];
 
 export default pages;
