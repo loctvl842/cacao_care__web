a4cc6b3f71a2e622f359341d39e1bddee7fa537b
loctvl842 <loclepnvx@gmail.com>
2023-03-30 18:18:21 +0700

    chore: check `user` enter `Home`

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,15 +3,27 @@ import classNames from "classnames/bind";
 
 // components
 import { Sidebar, Chart, Rightbar } from "~/components";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 let cx = classNames.bind(styles);
 
 const Home = () => {
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+  useEffect(() => {
+    if (user === null) navigate("/login");
+  }, [user, navigate]);
   return (
     <div className={cx("container")}>
       <div className={cx("wrapper")}>
         <Sidebar />
-        <Chart />
-        <Rightbar />
+        {user !== null && (
+          <>
+            <Chart />
+            <Rightbar />
+          </>
+        )}
       </div>
     </div>
   );
