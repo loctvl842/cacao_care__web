3fcafad8a135dcfee1992448100236798ca9584b
loctvl842 <loclepnvx@gmail.com>
2023-03-30 18:17:44 +0700

    chore: split the fetch job in function

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -40,6 +40,22 @@ const options = {
   },
 };
 
+const fetchFeedData = async (user) => {
+  try {
+    const res = await axios.get(
+      `https://io.adafruit.com/api/v2/${user.username}/feeds/dht20-humi/data?limit=10`,
+      {
+        headers: {
+          "X-AIO-Key": user.active_key,
+        },
+      }
+    );
+    return res.data.reverse();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 let cx = classNames.bind(styles);
 
 let labels = [];
@@ -58,39 +74,26 @@ const Chart = () => {
   });
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          `https://io.adafruit.com/api/v2/${user.username}/feeds/dht20-humi/data?limit=10`,
-          {
-            headers: {
-              "X-AIO-Key": user.active_key,
-            },
-          }
-        );
-        const resData = res.data.reverse();
-        setData({
-          labels: resData.map((d) => {
-            const createdDate = d.created_at;
-            const date = new Date(createdDate);
-            const m = date.getUTCMinutes();
-            const s = date.getUTCSeconds();
-            return m + ":" + s;
-          }),
-          datasets: [
-            {
-              label: resData[0].feed_key,
-              data: resData.map((d) => d.value),
-              borderColor: "#DFA67B",
-              backgroundColor: "#DFA67B",
-            },
-          ],
-        });
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    fetchData();
+    if (user === null) return;
+    const data = fetchFeedData(user);
+
+    setData({
+      labels: data.map((d) => {
+        const createdDate = d.created_at;
+        const date = new Date(createdDate);
+        const m = date.getUTCMinutes();
+        const s = date.getUTCSeconds();
+        return m + ":" + s;
+      }),
+      datasets: [
+        {
+          label: data[0].feed_key,
+          data: data.map((d) => d.value),
+          borderColor: "#DFA67B",
+          backgroundColor: "#DFA67B",
+        },
+      ],
+    });
   }, [user]);
 
   return (
